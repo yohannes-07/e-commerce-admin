77c2e097a725d90c7b5ab3fa2c5b8fba3261e6e1
Yohannes Teffera <yohannesahunm7@gmail.com>
2023-09-23 18:45:26 +0300

    Fix Order page

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -1,30 +1,23 @@
-'use client'
+"use client";
 
+import { DataTable } from "@/components/ui/data-table";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { OrderColumn, columns } from "./columns";
-import { DataTable } from "@/components/ui/data-table";
 
+import { columns, OrderColumn } from "./columns";
 
 interface OrderClientProps {
-  data:OrderColumn[]
+  data: OrderColumn[];
 }
 
-const OrderClient:React.FC<OrderClientProps> = ({
+export const OrderClient: React.FC<OrderClientProps> = ({
   data
 }) => {
-   
   return (
     <>
-      <Heading
-          title={`Orders (${data.length})`}
-          description="Manage orders for you store"
-        />
+      <Heading title={`Orders (${data.length})`} description="Manage orders for your store" />
       <Separator />
-      <DataTable  columns={columns} data={data} searchKey="products" />
-    
+      <DataTable searchKey="products" columns={columns} data={data} />
     </>
-  )
-}
-
-export default OrderClient;
+  );
+};
--- app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -3,13 +3,13 @@
 import { ColumnDef } from "@tanstack/react-table"
 
 export type OrderColumn = {
-  id: string
-  phone: string
-  address:string
-  isPaid:boolean
-  totalPrice:string
-  products:string
-  createdAt: string
+  id: string;
+  phone: string;
+  address: string;
+  isPaid: boolean;
+  totalPrice: string;
+  products: string;
+  createdAt: string;
 }
 
 export const columns: ColumnDef<OrderColumn>[] = [
@@ -27,10 +27,10 @@ export const columns: ColumnDef<OrderColumn>[] = [
   },
   {
     accessorKey: "totalPrice",
-    header: "TotalPrice",
+    header: "Total price",
   },
   {
     accessorKey: "isPaid",
     header: "Paid",
   },
-]
+];
